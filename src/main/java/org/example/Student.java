c48cdcc0bc780c0a522812a28a34ec854672c5d0
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-01-26 15:46:50 +0530

    second commit

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,48 +1,50 @@
 package org.example;
 import java.util.Scanner;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Student{
-    String StudentName;
-    char GradeLevel;
-    float GPA;
+    private static final Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+   private String studentName;
+    private char gradeLevel;
+    private float gPA;
     Student(String name,char gr,float gp){
-        StudentName=name;
-        GradeLevel=gr;
-        GPA=gp;
+        studentName=name;
+        gradeLevel=gr;
+        gPA=gp;
     }
-    public void Update(float gpa){
-        GPA=gpa;
-        System.out.println("The updated Gpa is-----"+GPA);
+    public void update(float gpa){
+        gPA=gpa;
+        LOGGER.log(Level.INFO,"The updated Gpa is-----"+gPA);
     }
-    public String NameandGPA(){
+    public String nameAndgPA(){
         String res="";
-        res+=StudentName+" "+"has a "+GPA+" GPA";
+        res+=studentName+" "+"has a "+gPA+" GPA";
         return res;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Please Enter the Student Name");
+        LOGGER.log(Level.INFO,"Please Enter the Student Name");
         String name=sc.next();
-        System.out.println("Enter the Grade level");
+        LOGGER.log(Level.INFO,"Enter the Grade level");
         char gr = sc.next().charAt(0);
-        System.out.println("Enter your GPA");
+        LOGGER.log(Level.INFO,"Enter your GPA");
         float gp=sc.nextFloat();
         Student calculation = new Student(name, gr, gp);
         int choose=0;
         while(choose!=4){
-            System.out.println("Do you need to update your GPA"+"\n"+"1.Yes"+"\n"+"2.No"+"\n"+"3.View Student Name and GPA"+"\n"+"4.Exit");
+            LOGGER.log(Level.INFO,"Do you need to update your GPA"+"\n"+"1.Yes"+"\n"+"2.No"+"\n"+"3.View Student Name and GPA"+"\n"+"4.Exit");
             choose=sc.nextInt();
             if(choose==1){
-                System.out.println("Enter the GPA to be Upated");
+                LOGGER.log(Level.INFO,"Enter the GPA to be Upated");
                 float updated=sc.nextFloat();
-                calculation.Update(updated);
+                calculation.update(updated);
             }
             else if(choose==2){
-                System.out.println("Its Okay");
+                LOGGER.log(Level.INFO,"Its Okay");
             }
             else if(choose==3){
-                String result=calculation.NameandGPA();
-                System.out.println(result);
+                String result=calculation.nameAndgPA();
+                LOGGER.log(Level.INFO,result);
             }
         }
     }
